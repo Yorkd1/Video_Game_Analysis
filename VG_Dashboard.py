271c180c4f2ef31b9b49fa3e66f3149fcc72bc48
David York <yor15001@byui.edu>
2025-02-08 20:15:15 -0700

    Made better comments for future self.

--- VG_Dashboard.py
+++ VG_Dashboard.py
@@ -7,7 +7,7 @@ from dash import dcc, html, dash_table
 # Load dataset
 df = pd.read_csv('Video_Games.csv')
 
-# Drop the duplicates from the data set.
+# Drop the duplicates from the data set for clean data frame
 df_cleaned = df.drop_duplicates(subset=['Title'], keep='first')
 
 """
@@ -25,7 +25,7 @@ Platinum: #E0E1DD
 pokemon_games = df_cleaned[df_cleaned['Title'].str.contains(r'pok[eé]mon', case=False, na=False)]
 top_10_pokemon_games = pokemon_games.sort_values(by='Rating', ascending=False).head(10)
 
-# Rename game titles to remove "Pokemon"
+# Rename game titles to remove "Pokemon" to reduce redundancy.
 top_10_pokemon_games['Title'] = top_10_pokemon_games['Title'].str.replace(r'pok[eé]mon ', '', case=False, regex=True)
 
 fig_pokemon = go.Figure()
@@ -52,7 +52,7 @@ Question 2: What is the top most played games?
 
 """
 
-# Extract data about top played games in 2023
+# Extract data about top played games also replacing 'K' with '000'
 df_cleaned['Plays'] = df_cleaned['Plays'].astype(str).str.replace('K', '000', regex=True).astype(float)
 top_games = df_cleaned.sort_values(by='Plays', ascending=False).head(10)
 
@@ -63,16 +63,19 @@ fig_top_games.update_layout(xaxis_tickangle=-45,
                             paper_bgcolor='#778DA9',
                             plot_bgcolor='#778DA9')
 
-# Create a sample table
+# Create a sample table with reduced columns of original data set. Summaries and Reviews will be to long to view
 df_table = df_cleaned[['Title', 'Release Date', 'Team', 'Rating', 'Genres', 'Plays', 'Playing']]
 
 # Initialize Dash app
 app = dash.Dash(__name__)
 
-# Define layout
+# Define layout for Dashboard.
 app.layout = html.Div(children=[
+
+    # Title at the very top of Dashboard
     html.H1("Video Game Data Dashboard", style={'textAlign': 'center', 'color': 'white'}),
     
+    # Sample table of data
     html.Div([
         html.H2("Video Game Data Table", style={'textAlign': 'center', 'color': 'white'}),
             dash_table.DataTable(
@@ -85,12 +88,14 @@ app.layout = html.Div(children=[
         )
     ], style={'backgroundColor': '#415A77', 'padding': '20px', 'borderRadius': '10px', 'margin-top': '50px', 'margin-bottom': '20px'}),
     
-
+    # Creates a bar chart for viewing the highest rated pokemon games
     html.Div([
         html.H2("Highest Rated Pokémon Games", style={'textAlign': 'center', 'color': 'white'}),
         dcc.Graph(figure=fig_pokemon),
     ], style={'backgroundColor': '#415A77', 'padding': '20px', 'borderRadius': '10px', 'margin-bottom': '20px'}),
 
+    # Produces the top most played games
+    # ** Redo this to show popularity calculation** 
     html.Div([
         html.H2("Top 10 Most Played Games", style={'textAlign': 'center', 'color': 'white'}),
         dcc.Graph(figure=fig_top_games),
